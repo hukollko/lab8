3933105d99fde22099aa3b0204281d1d9196e775
Вероника Бертенева <veronicaberteneva@gmail.com>
2023-09-30 08:27:40 +0300

    Дни и подсчет секунд

--- time.pas
+++ time.pas
@@ -1,7 +1,7 @@
 ﻿begin
   var g := ReadInteger;
-  var o := 'Год не високосный';
+  var o := 365;
   if (g mod 4 = 0) and ((g mod 400 = 0) or (g mod 100 <> 0)) then
-    o := 'Год високосный';
+    o := 366;
   print(o);
 end.
---
+++ timer.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var h := ReadInteger;
+  var m := ReadInteger;
+  var s := ReadInteger;
+  assert((h>=0) and (m>=0) and (s>=0));
+  var sum := 0;
+  sum := h*3600 + m*60 + s;
+  print(sum);
+end.
