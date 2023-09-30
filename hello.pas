981b827b6d0fbe2947103da2a7afb0c0c6737d54
Вероника Бертенева <veronicaberteneva@gmail.com>
2023-09-30 08:03:32 +0300

    time

--- hello.pas
+++ hello.pas
@@ -1,3 +1,12 @@
 ﻿begin
-  print('Привет, мир!');
+  var c := ReadInteger;
+  assert((c>=0) and (c<=23));
+  var say := '';
+  case c of
+    4..10: say:= 'Доброе утро, мир!';
+    11..16: say:= 'Добрый день, мир!';
+    17..22: say:= 'Добрый вечер, мир!';
+    else say := 'Доброй ночи, мир!'
+  end;
+  print(say);
 end.
